1b6481f1b0815ed89481c697f5cd9aae0c75d902
Isaac Velando <isaac@greplinux.com>
2021-01-16 19:21:01 -0600

    fix defer setting and add some explanations

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -36,7 +36,7 @@ func Scrape(logger *zap.Logger, conf config.Configuration) (*ModemInformation, e
 	}
 	eventLog := scrapeEventLogs(logger, doc)
 
-	// Logout to let the modem reclaim resources
+	// Logout to let the modem reclaim resources, per https://github.com/mdonoughe/modem_status
 	getDocumentFromURL(logger, conf.Modem.Url+"/logout.html", conf)
 
 	modemInformation := ModemInformation{
@@ -55,11 +55,11 @@ func getDocumentFromURL(logger *zap.Logger, address string, conf config.Configur
 
 	start := time.Now()
 
-	// TODO: add a timeout here (10s?)
 	token, err := getToken(logger, conf)
 	if err != nil {
 		return nil, err
 	}
+
 	jar, _ := cookiejar.New(nil)
 	var cookies []*http.Cookie
 	cookie := &http.Cookie{
@@ -71,21 +71,26 @@ func getDocumentFromURL(logger *zap.Logger, address string, conf config.Configur
 	cookies = append(cookies, cookie)
 	u, _ := url.Parse(address)
 	jar.SetCookies(u, cookies)
+
+	// The modem has an ancient cert loaded and there is no option to replace it
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	client := &http.Client{
 		Jar: jar,
 	}
+
 	req, err := http.NewRequest("GET", address, nil)
 	if err != nil {
 		return nil, err
 	}
 	req.SetBasicAuth(conf.Modem.Username, conf.Modem.Password)
+
 	resp, err := client.Do(req)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
-
 	if resp.StatusCode != 200 {
 		return nil, fmt.Errorf("status code error: %d %s", resp.StatusCode, resp.Status)
 	}
@@ -104,23 +109,31 @@ func getDocumentFromURL(logger *zap.Logger, address string, conf config.Configur
 }
 
 func getToken(logger *zap.Logger, conf config.Configuration) (string, error) {
+
+	// The modem has an ancient cert loaded and there is no option to replace it
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	client := &http.Client{}
+
 	authString := conf.Modem.Username + ":" + conf.Modem.Password
 	basicAuthString := base64.StdEncoding.EncodeToString([]byte(authString))
+
 	req, err := http.NewRequest("GET", conf.Modem.Url+"/cmconnectionstatus.html?"+basicAuthString, nil)
 	if err != nil {
 		return "", err
 	}
 	req.SetBasicAuth(conf.Modem.Username, conf.Modem.Password)
+
 	resp, err := client.Do(req)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
 	if err != nil {
 		return "", err
 	}
-	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
 		return "", fmt.Errorf("status code error: %d %s", resp.StatusCode, resp.Status)
 	}
+
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
@@ -129,5 +142,6 @@ func getToken(logger *zap.Logger, conf config.Configuration) (string, error) {
 	if len(token) != 31 {
 		return "", fmt.Errorf("did not retrieve auth token successfully")
 	}
-	return string(bodyBytes), nil
+
+	return token, nil
 }
