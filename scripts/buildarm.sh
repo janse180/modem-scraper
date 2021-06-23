9c70a7680f905ed73e744df99073848ae7f8d89a
Matt Jansen <matt_jansen@trimble.com>
2021-06-23 17:26:49 -0500

    update for arm builds

---
+++ scripts/buildarm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BUILD_VERSION="$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)"
+
+echo "Building arm binary with version [${BUILD_VERSION}]..."
+
+BINARY_NAME="modem-scraper"
+FLAGS="-X main.BuildVersion=${BUILD_VERSION}"
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -ldflags="${FLAGS}" -o ${BINARY_NAME}
+
+ARCHIVE_NAME="${BINARY_NAME}_${BUILD_VERSION}_linux_arm64.zip"
+zip ${ARCHIVE_NAME} ${BINARY_NAME}
+
+if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" = "" ] ; then
+
+  echo "Building image..."
+  IMAGE="janse180/modem-scraper"
+  docker build . -t ${IMAGE}:${BUILD_VERSION}-arm64
+
+  echo "Pushing image to registry..."
+  echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin ${DOCKER_URL}
+  docker push ${IMAGE}:${BUILD_VERSION}-arm64
+fi
