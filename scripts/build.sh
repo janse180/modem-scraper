276b3371bda822bc19086a767d70807057e766fd
Matt Jansen <matt_jansen@trimble.com>
2021-06-23 16:51:24 -0500

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,11 +19,11 @@ if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" = "" ]
   git config --local user.name "${GIT_USER_NAME}"
   git config --local user.email "${GIT_USER_EMAIL}"
   git tag ${BUILD_VERSION}
-  git remote add tag-origin https://${GITHUB_TOKEN}@github.com/pdunnavant/modem-scraper.git
+  git remote add tag-origin https://${GITHUB_TOKEN}@github.com/janse180/modem-scraper.git
   git push tag-origin ${BUILD_VERSION}
 
   echo "Building image..."
-  IMAGE="pdunnavant/modem-scraper"
+  IMAGE="janse180/modem-scraper"
   docker build . -t ${IMAGE}:${BUILD_VERSION}
   docker tag ${IMAGE}:${BUILD_VERSION} ${IMAGE}:latest
 
